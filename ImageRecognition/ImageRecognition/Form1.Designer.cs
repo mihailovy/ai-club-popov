07855b57b9b3ffe93279a13b94de859250bdb017
Boyan Mihailov <mihailovy@gmail.com>
2025-01-15 11:11:06 +0200

    Initial code

---
+++ ImageRecognition/ImageRecognition/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace ImageRecognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(268, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 102);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Run Network";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(100, 182);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(341, 26);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(21, 50);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(73, 26);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(100, 50);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(73, 26);
+            this.textBox5.TabIndex = 5;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(179, 50);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(73, 26);
+            this.textBox6.TabIndex = 6;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(179, 88);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(73, 26);
+            this.textBox7.TabIndex = 9;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(100, 88);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(73, 26);
+            this.textBox8.TabIndex = 8;
+            this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(21, 88);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(73, 26);
+            this.textBox9.TabIndex = 7;
+            this.textBox9.TextChanged += new System.EventHandler(this.textBox9_TextChanged);
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(179, 126);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(73, 26);
+            this.textBox10.TabIndex = 12;
+            this.textBox10.TextChanged += new System.EventHandler(this.textBox10_TextChanged);
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(100, 126);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(73, 26);
+            this.textBox11.TabIndex = 11;
+            this.textBox11.TextChanged += new System.EventHandler(this.textBox11_TextChanged);
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(21, 126);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(73, 26);
+            this.textBox12.TabIndex = 10;
+            this.textBox12.TextChanged += new System.EventHandler(this.textBox12_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 20);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Image:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 20);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Output:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 229);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Simple Neural Network";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ImageRecognition/ImageRecognition/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageRecognition
+{
+
+    public partial class Form1 : Form
+    {
+        public double[][] img = new double[][]
+        {
+            new double[] {0     , 255.0  , 0.0},
+            new double[] {0.0   , 0.0    , 255.0},
+            new double[] {0.0   , 255.0  , 0.0}
+        };
+
+        private Neuron[] level1 = new Neuron[9];
+        private Neuron level2;
+        
+    public Form1()
+        {
+            InitializeComponent();
+            level1[0] = new Neuron(
+                new double[] { -1.0, -1.0, -1.0,
+                               -1.0, -1.0, -1.0,
+                               -1.0, -1.0, -1.0}
+            );
+            level1[1] = new Neuron(
+                new double[] { -1.0, +100.0, -1.0,
+                               -1.0, -1.0, -1.0,
+                               -1.0, -1.0, -1.0}
+            );
+            level1[2] = new Neuron(
+                new double[] { -1.0, -1.0, -1.0,
+                               -1.0, -1.0, -1.0,
+                               -1.0, -1.0, -1.0}
+            );
+            level1[3] = new Neuron(
+                 new double[] {-1.0, -1.0, -1.0,
+                               +100.0, -1.0, -1.0,
+                               -1.0, -1.0, -1.0}
+            );
+            level1[4] = new Neuron(
+                 new double[] {-1.0, -1.0, -1.0,
+                               -1.0, -1.0, -1.0,
+                               -1.0, -1.0, -1.0}
+            );
+            level1[5] = new Neuron(
+                 new double[] {-1.0, -1.0, -1.0,
+                               -1.0, -1.0, +100.0,
+                               -1.0, -1.0, -1.0}
+                 );
+            level1[6] = new Neuron(
+                 new double[] {-1.0, -1.0, -1.0,
+                               -1.0, -1.0, -1.0,
+                               -1.0, -1.0, -1.0}
+            );
+            level1[7] = new Neuron(
+                 new double[] {-1.0, -1.0, -1.0,
+                               -1.0, -1.0, -1.0,
+                               -1.0, +100.0, -1.0}
+            );
+            level1[8] = new Neuron(
+                 new double[] {-1.0, -1.0, -1.0,
+                               -1.0, -1.0, -1.0,
+                               -1.0, -1.0, -1.0}
+                 );
+            level2 = new Neuron(
+                 new double[] {-125.0, +1250.0, -125.0,
+                               +1250.0, -125.0, +1250.0,
+                               -125.0, +1250.0, -125.0}
+                 );
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double[] img2 = new double[9];
+            img2[0] = (double)img[0][0];
+            img2[1] = (double)img[0][1];
+            img2[2] = (double)img[0][2];
+            img2[3] = (double)img[1][0];
+            img2[4] = (double)img[1][1];
+            img2[5] = (double)img[1][2];
+            img2[6] = (double)img[2][0];
+            img2[7] = (double)img[2][1];
+            img2[8] = (double)img[2][2];
+            textBox4.Text = "" + img2[0];
+            textBox5.Text = "" + img2[1];
+            textBox6.Text = "" + img2[2];
+            
+            textBox9.Text = "" + img2[3];
+            textBox8.Text = "" + img2[4];
+            textBox7.Text = "" + img2[5];
+
+            textBox12.Text = "" + img2[6];
+            textBox11.Text = "" + img2[7];
+            textBox10.Text = "" + img2[8];
+
+            double[] outputLevel1 = new double[9];
+            for (int i = 0; i < img2.Length; i++) {
+                outputLevel1[i] = level1[i].Work(img2);
+            }
+            textBox3.Text = "N0=" + outputLevel1[0].ToString();
+            textBox3.Text += ",N1=" + outputLevel1[1].ToString();
+            textBox3.Text += ",N2=" + outputLevel1[2].ToString();
+            textBox3.Text += ",N3=" + outputLevel1[3].ToString();
+            textBox3.Text += ",N4=" + outputLevel1[4].ToString();
+            textBox3.Text += ",N5=" + outputLevel1[5].ToString();
+            textBox3.Text += ",N6=" + outputLevel1[6].ToString();
+            textBox3.Text += ",N7=" + outputLevel1[7].ToString();
+            textBox3.Text += ",N8=" + outputLevel1[8].ToString();
+
+
+            double output = level2.Work(outputLevel1);
+            textBox3.Text += ",O=" + output.ToString();
+        }
+         
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox12_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox11_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox10_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    class Neuron
+    {
+        private int number;
+        private double[] input;
+        private double[] weight;
+        public double mid;
+
+        public Neuron(double[] w)
+        {
+            weight = w;
+            number = weight.Length;
+        }
+
+        public double Work(double[] inp)
+        {
+            double sum = 0;
+            double output;
+            input = inp;
+            for (int i = 0; i < input.Length; i++)
+            {
+                sum += input[i] * weight[i];
+            }
+            mid = sum;
+            output = 1 / (1 + Math.Exp(-sum));
+            return output;
+        }
+
+    }
+}
